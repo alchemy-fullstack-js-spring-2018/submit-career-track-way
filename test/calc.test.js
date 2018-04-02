f548b8ebaac4b2bd58d71bda15b4f3bf38ea75a4
Tasha <tashazun@gmail.com>
2018-04-02 13:20:53 -0700

    divide by 0

--- test/calc.test.js
+++ test/calc.test.js
@@ -21,4 +21,13 @@ describe('calculator', () => {
         const quotient = calc.divide(4, 2);
         assert.equal(quotient, 2);
     })
+
+    it('divide by 0', () => {
+        try{
+            calc.divide(4, 0);
+            assert.fail('did not throw error');
+        } catch (error) {
+            assert.equal(error.message, 'Attempt to divide by zero (0)');
+        }
+    })
 });
