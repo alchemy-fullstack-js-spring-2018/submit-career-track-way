c563abe7e0e34cf8165e2b78a4053b7e5e197668
Jack Lomax <lomax15@me.com>
2018-04-02 12:13:54 -0700

    functional add, substract, multiply, divide with tests

--- test/calc.test.js
+++ test/calc.test.js
@@ -6,4 +6,19 @@ describe ('calculator', () => {
         const sum = calc.add(1, 2);
         assert.equal(sum, 3);
     });
+
+    it('subtract', () => {
+        const sub = calc.subtract(2, 1);
+        assert.equal(sub, 1);
+    });
+
+    it('multiply', () => {
+        const mult = calc.multiply(2, 2);
+        assert.equal(mult, 4);
+    });
+    
+    it('divide', () => {
+        const div = calc.divide(4, 2);
+        assert.equal(div, 2);
+    });
 });
