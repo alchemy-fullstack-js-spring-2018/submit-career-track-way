1544717c1855511d7401dbed3b9e43bace72019d
Steele Walston <steele.walston@gmail.com>
2018-04-02 11:48:32 -0700

    add test works

--- test/clac.test.js
+++ test/clac.test.js
@@ -1,8 +1,9 @@
 const assert = require('assert');
+const calc = require('../lib/calc');
 
 describe('calculator', () => {
     it('add', () => {
-
-
+        const sum = calc.add(1, 2);
+        assert.equal(sum, 3);
     });
 });
