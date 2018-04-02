298f9a8a9090be5b05b24090db53c91aba008b48
J3ffcon1 <Jrlone1990@gmail.com>
2018-04-02 12:02:38 -0700

    test #2

--- test/calc.test.js
+++ test/calc.test.js
@@ -8,9 +8,19 @@ describe('calculator', () => {
     it('add', () => {
         //excercising the code we want to test.
         //in TDD also the code we want to write.
-    const sum = calc.add(1,2);
-     //make an assertion about what is "pass" What passes the test.
-      // parameters: actual, expected.
-      assert.equal(sum, 3);
-    } );
+        const sum = calc.add(1,2);
+        //make an assertion about what is "pass" What passes the test.
+        // parameters: actual, expected.
+        assert.equal(sum, 3);
+    });
+
+    it('subtract', () => {
+        const subtract = calc.subtract(5, 4);
+        assert.equal(subtract, 1);
+    });
+    it('multiply', () => {
+        const multiply = calc.multiply(5, 4);
+        assert.equal(multiply, 20);
+    });
+
 });
