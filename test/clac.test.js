c97eee742cc41ffcac08e174b08a592bd28cf09b
Steele Walston <steele.walston@gmail.com>
2018-04-02 12:03:37 -0700

    all tests working

--- test/clac.test.js
+++ test/clac.test.js
@@ -5,5 +5,17 @@ describe('calculator', () => {
     it('add', () => {
         const sum = calc.add(1, 2);
         assert.equal(sum, 3);
+    }),
+    it('subtract', () => {
+        const diff = calc.subtract(2, 1);
+        assert.equal(diff, 1);
+    }),
+    it('multiply', () => {
+        const product = calc.multiply(2, 2);
+        assert.equal(product, 4);
+    }),
+    it('divide', () => {
+        const quotient = calc.divide(4, 2);
+        assert.equal(quotient, 2);
     });
 });
