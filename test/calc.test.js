e3892b45975bbae27b19ad146545f3b624d19973
victorbofill <victorm.bofill@gmail.com>
2018-04-02 11:48:32 -0700

    "add" functionality

--- test/calc.test.js
+++ test/calc.test.js
@@ -1,7 +1,18 @@
 const assert = require('assert');
+const calc = require('../lib/calc');
 
+// "suite" feature for tests. 'it' will be
+// outlined under this label
 describe('calculator', () => {
+
+    // 'it' is mocha's way of "registering" a test
+    // function
     it('add', () => {
-        
+        // exercising the code we want to test
+        // (in TDD also the code we want to write)
+        const sum = calc.add(1, 2);
+        // make an assertion about what is "pass"
+        // parameters: actual, expected
+        assert.equal(sum, 3);
     });
 });
