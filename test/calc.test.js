eb35041fb91e928f405b0f57c2a925682ee90f7f
Charlie <heinercj@gmail.com>
2018-04-02 13:06:31 -0700

    subtract, multiply, divide and tests for each

--- test/calc.test.js
+++ test/calc.test.js
@@ -3,7 +3,32 @@ const calc = require('../lib/calc');
 
 describe('calculator', () => {
     it('add', () => {
-        const sum = calc.add(1,2);
+        const sum = calc.add(1, 2);
         assert.equal(sum, 3);
+    });
+
+    it('subtract', () => {
+        const diff = calc.subtract(5, 3);
+        assert.equal(diff, 2); 
+    });
+
+    it('mulitiplies', () => {
+        const total = calc.multiply(5, 5);
+        assert.equal(total, 25); 
+    });
+    
+    it('divides', () => {
+        const answer = calc.divide(10, 2);
+        assert.equal(answer, 5);
+    });
+
+    it('dividing by zero throws error', () => {
+       
+        try { 
+            calc.divide(10, 0);
+        }
+        catch (error){
+            assert.equal(error.message, 'attempted to divide by zero');
+        }
     });
 });
