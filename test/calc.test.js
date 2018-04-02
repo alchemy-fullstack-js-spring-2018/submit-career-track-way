5654f3ab86321fc84075a73c354c6a56f52fa58e
pereztjacob <pereztjacob@gmail.com>
2018-04-02 12:14:27 -0700

    divide by 0

--- test/calc.test.js
+++ test/calc.test.js
@@ -3,7 +3,12 @@ const calc = require('../lib/calc');
 
 describe('calculator', () => {
     it('add', () => {
-        const sum = calc(1, 2);
+        const sum = calc.add(1, 2);
         assert.equal(sum, 3);
+    });
+
+    it('divide by zero', () => {
+        const result = calc.divide(5, 0);
+        assert.equal(result, Infinity);
     });
 });
