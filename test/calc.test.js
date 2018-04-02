e6cc85129e55dbb611bc75c23a115ef37c4598e1
Ryan Manro <rmanro@gmail.com>
2018-04-02 11:59:36 -0700

    all math functions working

--- test/calc.test.js
+++ test/calc.test.js
@@ -6,4 +6,19 @@ describe('calculator', () => {
         const sum = calc.add(1, 2);
         assert.equal(sum, 3);
     });
+
+    it('subtract', () => {
+        const difference = calc.subtract(2, 1);
+        assert.equal(difference, 1);
+    });
+
+    it('multiply', () => {
+        const product = calc.multiply(1, 2);
+        assert.equal(product, 2);
+    });
+
+    it('divide', () => {
+        const quotient = calc.divide(2, 1);
+        assert.equal(quotient, 2);
+    });
 });
